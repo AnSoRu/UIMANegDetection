e710c9820cbf67de5a7e873a873546aa0bef7eb3
Angel <angelsoler91@gmail.com>
2018-04-04 00:30:03 +0200

    Primeras Pruebas
    Falla porque dice que no encuentra la clase
    org.apache.uima.annotators.NoDetectorAnnotator

---
+++ src/org/apache/uima/annotators/NoDetectorAnnotator.java
@@ -0,0 +1,26 @@
+package org.apache.uima.annotators;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.defecto.NoDetector;
+import org.apache.uima.jcas.JCas;
+
+public class NoDetectorAnnotator extends JCasAnnotator_ImplBase {
+
+	private Pattern noPattern = Pattern.compile(".*(No|no).*");
+
+	@Override
+	public void process(JCas jCas) throws AnalysisEngineProcessException {
+		String docText = jCas.getDocumentText();
+		Matcher matcher = noPattern.matcher(docText);
+		while(matcher.find()) {
+			NoDetector noAnnotation = new NoDetector(jCas);
+			noAnnotation.setBegin(matcher.start());
+			noAnnotation.setEnd(matcher.end());
+			noAnnotation.addToIndexes();
+		}
+	}
+
+}
---
+++ src/org/apache/uima/defecto/NoDetector_Type.java
@@ -0,0 +1,39 @@
+package org.apache.uima.defecto;
+
+/* First created by JCasGen Tue Apr 03 22:41:57 CEST 2018 */
+
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.Type;
+import org.apache.uima.jcas.tcas.Annotation_Type;
+
+/** 
+ * Updated by JCasGen Tue Apr 03 23:10:40 CEST 2018
+ * @generated */
+public class NoDetector_Type extends Annotation_Type {
+  /** @generated */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = NoDetector.typeIndexID;
+  /** @generated 
+     @modifiable */
+  @SuppressWarnings ("hiding")
+  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("NoDetector");
+
+
+
+  /** initialize variables to correspond with Cas Type and Features
+	 * @generated
+	 * @param jcas JCas
+	 * @param casType Type 
+	 */
+  public NoDetector_Type(JCas jcas, Type casType) {
+    super(jcas, casType);
+    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
+
+  }
+}
+
+
+
+    
