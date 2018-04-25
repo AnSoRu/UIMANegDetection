b70453644f2f72d1666097ddb99308e91e2f9144
Angel <angelsoler91@gmail.com>
2018-04-25 22:12:48 +0200

    Evitar anotar más de una vez
    Se ha añadido una última parte de código para evitar coger anotaciones
    que estén contenidas en otras. Ej: sin signos de. El anotador puesto que
    está basado en diccionario va a encontrar 3 anotaciones: sin, sin
    signos, sin signos de. La que me vale de verdad es la que tenga mayor
    longitud, (sin signos de). Por tanto tengo que comparar las 3 y quedarme
    sólo con la mayor, eliminando las otras dos, pues están contenidas en
    esta.

--- src/NegationAnalyzer.java
+++ src/NegationAnalyzer.java
@@ -2,7 +2,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.uima.UIMAFramework;
@@ -24,7 +26,7 @@ import defecto.NoDetector;
 public class NegationAnalyzer {
 
 	//Clase que representa al programa
-	
+
 	//Tabla para almacenar de cada oración las anotaciones que tienen asociadas para facilitar posteriormente
 	//la búsqueda
 	//La clave es el id de la oración y el valor almacenado es una lista de las anotaciones que hay en esa oración
@@ -48,14 +50,14 @@ public class NegationAnalyzer {
 			fis.read(contents);
 			fis.close();
 			String document = new String(contents);
-			
+
 			anotaciones = new HashMap<Integer,List<NoDetector>>();
 
 			tCas.setDocumentText(document);
 			tae.process(tCas);
 
 			storeAnnotations(tCas);
-			
+
 			tae.destroy();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -67,28 +69,59 @@ public class NegationAnalyzer {
 			e.printStackTrace();
 		}
 	}//end Constructor
-	
+
 	private static void storeAnnotations(JCas jcas) {
 		Type tipo = jcas.getTypeSystem().getType("defecto.NoDetector");
 		FSIterator<Annotation> iter = jcas.getAnnotationIndex(tipo).iterator();
 		while(iter.isValid()) {
 			FeatureStructure fs = iter.get();
-			NoDetector annot = (NoDetector)fs;
+			NoDetector annot = (NoDetector)fs; //Esta es la anotación que quiero añadir
 			//System.out.println("Anotación (Covered Text) > " + annot.getCoveredText());
 			System.out.println("Comienzo de anotación " + annot.getBegin() + " hasta " + annot.getEnd() + " ID de la oración "+ annot.getIdOracion());
 			System.out.println("Oración " + annot.getOracionString() );
 			//anotaciones.add(index, element);
-			List<NoDetector> aux = anotaciones.get(annot.getIdOracion());
+			List<NoDetector> aux = anotaciones.get(annot.getIdOracion()); //Estas son las anotaciones de la oración
 			if(aux==null) {
 				//Creo una nueva lista
 				List<NoDetector> nuevaLista = new ArrayList<NoDetector>();
 				nuevaLista.add(annot);
 				anotaciones.put(annot.getIdOracion(),nuevaLista);
 			}else {
-				aux.add(annot);
+				//No puedo evitar que se cree una anotación que esté contenida en otra de mayor tamaño, pues al estar basado en un
+				//diccionario va a anotarlo. Ej: sin signos de. Va a anotar 3 veces (sin signos de, sin signos y sin). Y es parcialmente
+				//correcto. Me quedo con la de mayor tamaño
+				//Para evitar que me anote varias veces una palabra que ya está contenida en un conjunto de palabras
+				//de las cuales ya tengo la anotación tengo que filtrar
+				boolean add = false;
+				for(NoDetector nD: aux) { //nD es la anotación que ya tengo
+					//annot es la que quiero añadir o no
+					if(nD.getBegin() == annot.getBegin()) { //deben de comenzar en el mismo caracter
+						if(nD.getEnd() < annot.getEnd()) { //este caso se correspondería con nD = sin y annot = sin signos de
+							//Debo eliminar las anotación que tengo (nD) 
+							aux.remove(nD);
+							add = true; //Indico que tengo que añadir la nueva annot (sin signos de)
+							break;
+							//aux.add(annot);
+						}
+					}
+				}
+				if(add) {
+					aux.add(annot);
+				}
 				anotaciones.put(annot.getIdOracion(),aux);
 			}
 			iter.moveToNext();
+		}
+		Collection<List<NoDetector>>anAux = anotaciones.values();
+		Iterator<List<NoDetector>> it = anAux.iterator();
+		System.out.println("###########################");
+		System.out.println("Despues de filtrar");
+		while(it.hasNext()) {
+			List<NoDetector> listaAux = it.next();
+			for(NoDetector annot : listaAux) {
+				System.out.println("Comienzo de anotación " + annot.getBegin() + " hasta " + annot.getEnd() + " ID de la oración "+ annot.getIdOracion());
+				System.out.println("Oración " + annot.getOracionString() );
+			}
 		}
 	}
 
@@ -132,10 +165,10 @@ public class NegationAnalyzer {
 		}
 		return res;
 	}
-	
+
 	public static void main(String [] args) {
 		NegationAnalyzer nA = new NegationAnalyzer();
-		System.out.println(nA.isNegated("enfermedad",2));
+		System.out.println(nA.isNegated("VIH",1));
 	}
 
 }
